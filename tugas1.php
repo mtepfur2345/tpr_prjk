5181c33dd663b171abcbfe7a8e8c2c431aeda836
mtepfur2345 <muhamadtefurnur@gmail.com>
2024-11-25 17:47:45 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,48 @@
+<?php
+// for ($i = 1;$i < 10;$i++) {
+// if (i($i%2)) {
+// continue;
+// }
+// echo "\$i = $i <BR>";
+// }
+?>
+
+<?php
+$a = 4;
+$b = 2;
+
+for ($i = 0; $i < 10; $i++) {
+    echo $a . "\n";
+    $a += $b;
+    $b++;
+}
+?>
+
+<?php
+echo "<br>";
+?>
+
+<?php
+
+for ($i = 2; $i <= 6; $i++) {
+    echo $i . "\n";
+    echo $i . "\n";   
+}
+?>
+
+<?php
+echo "<br>"
+?>
+
+<?php
+for ($i = 1; $i <= 3; $i++) {
+    echo $i . " ";
+    if ($i == 1) {
+        echo "9 ";
+    }elseif ($i == 2) {
+        echo "10 ";
+    }elseif ($i == 3) {
+        echo "11 ";
+    }
+}
+?>1
